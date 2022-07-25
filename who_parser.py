ebe66bbb9ccc7ca3473b66192013d447b5d7bc75
David Fortino <davidf@internal.county.domain>
2022-07-25 15:38:42 -0700

    Updated MainWindow variables

--- who_parser.py
+++ who_parser.py
@@ -21,6 +21,35 @@ class WorkerSignals(QObject):
     result = Signal(object)
 
 
+class Watcher:
+
+    def __init__(self, response_function, directory):
+        self.observer = Observer()
+        self.response_function = response_function
+        self.directory = directory
+
+    def run(self):
+        self.observer.schedule(self.response_function,
+                               self.directory, recursive=False)
+        self.observer.start()
+        try:
+            while self.observer.is_alive():
+                self.observer.join(1)
+
+        except:
+            self.observer.stop()
+            self.observer.join()
+            return 0
+        return 1
+
+    def stop(self):
+        if self.observer.is_alive():
+            self.observer.stop()
+            self.observer.join()
+            return 1
+        return 0
+
+
 class Thread(QThread):
     """
     Worker thread
@@ -32,7 +61,6 @@ class Thread(QThread):
         self.fn = fn
         self.args = args
         self.kwargs = kwargs
-        self.signals = WorkerSignals()
 
     @Slot()
     def run(self):
@@ -42,50 +70,81 @@ class Thread(QThread):
         # Retrieve args/kwargs here; and fire processing using them
         try:
             print("start of thread")
+            # watch directory
             result = self.fn(*self.args, **self.kwargs)
+
             print("end of thread", result)
         except:
             traceback.print_exc()
             exctype, value = sys.exc_info()[:2]
-            self.signals.error.emit((exctype, value, traceback.format_exc()))
-
-        else:
-            self.signals.result.emit(result)
 
 
-class Watcher:
+class Watch_Directory_Thread(Thread):
 
-    def __init__(self, directory, handler):
-        self.observer = Observer()
+    def __init__(self, directory, signals=None, *args, **kwargs):
+        if not signals:
+            raise ValueError('signals must be passed')
+        super().__init__(self.watch_directory, signals=signals, *args, **kwargs)
         self.directory = directory
-        self.handler = handler
 
-    def run(self):
-        self.observer.schedule(self.handler, self.directory, recursive=False)
-        self.observer.start()
-        try:
-            while self.observer.is_alive():
-                self.observer.join(1)
-
-        except:
-            self.observer.stop()
-            self.observer.join()
+    def watch_directory(self, signals=None):
+        self.watcher = Watcher(FileOnModifiedHandler(signals), self.directory)
+        print(f"start watch on {self.directory}")
+        res = self.watcher.run()
+        print("watcher stoped")
+        return res
 
 
-class MyHandler(FileSystemEventHandler):
+class FileOnModifiedHandler(FileSystemEventHandler):
+
+    def __init__(self, signals):
+        self.signals = signals
 
     def on_modified(self, event):
-        self.source_path = event.src_path
-        self.file_name = self.source_path.split('\\')[-1]
-        return self.file_name
+        self.file_name = event.src_path.split('\\')[-1]
+        # print(f'file modified: {self.file_name}')
+        if self.signals and self.file_name != 'dbg.txt':
+            self.signals.result.emit(self.file_name)
+            # print(f'emitting file modified: {self.file_name}')
+
+
+class File_Stream_Thread(Thread):
+
+    def __init__(self, current_file, signals, *args, **kwargs):
+        super().__init__(self.log_lines, signals=signals, *args, **kwargs)
+        self.current_file = current_file
+        self.directory = DIRECTORY
+        self.signals = signals
+
+    def log_lines(self):
+        self.logfile = open(self.directory + '\\' + self.current_file, 'r')
+        loglines = self.logtail(self.logfile)
+
+        for line in loglines:
+            self.signals.result.emit(line)
+
+    def logtail(self, logfile):
+        thefile = logfile
+        thefile.seek(0, 2)
+        while True:
+            line = thefile.readline()
+            if not line:
+                time.sleep(0.1)
+                continue
+            yield line
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.directory_watch_thread = Thread(self.watch_directory)
-        self.directory_watch_thread.start()
+        self.directory = DIRECTORY
+        self.file = None
+        self.watch_directory_signals = WorkerSignals()
+        self.watch_directory_signals.result.connect(self.file_compare)
+        self.watch_directory_thread = Watch_Directory_Thread(
+            self.directory, signals=self.watch_directory_signals)
+        self.watch_directory_thread.start()
 
         self.setWindowTitle("Who Parser")
 
@@ -104,81 +163,35 @@ class MainWindow(QMainWindow):
 
         self.show()
 
-    def watch_directory(self):
-        print("in watch_directory")
-        self.directory = DIRECTORY
-        self.event_handler = MyHandler()
-        self.watcher = Watcher(self.directory, self.event_handler)
-        self.watcher.run()
-        print("after watcher.run")
+    def file_compare(self, file):
+        if self.file != file:
+            self.file = file
+            self.start_file_stream(self.file)
 
-    def set_text(self, current_file):
-        print("in set_text")
-        print(current_file)
-        # textbox = self.editor.toPlainText()
-        # if textbox == "":
-        #     self.logfile = open(self.log_file, "r")
-        #     loglines = self.log_tail(self.logfile)
-        #     for line in loglines:
-        #         print(line)
+    def set_text(self, line):
+        new_line = line
+        current_text = self.editor.toPlainText()
+        updated_text = current_text + new_line
+        self.editor.setPlainText(updated_text)
+
+    def start_file_stream(self, new_file):
+        self.file = new_file
+        self.file_stream_signals = WorkerSignals()
+        self.file_stream_signals.result.connect(self.set_text)
+        self.file_stream_thread = File_Stream_Thread(
+            self.file, self.file_stream_signals)
+        self.file_stream_thread.start()
 
     def closeEvent(self, event):
         # events to trigger when app is closed out
-        self.watcher.observer.stop()
+        self.watch_directory_thread.terminate()
+        self.file_stream_thread.terminate()
 
     def clear_text(self):
         self.editor.setPlainText("")
-        self.set_text()
+        self.start_file_stream()
 
 
 app = QApplication(sys.argv)
 window = MainWindow()
 sys.exit(app.exec())
-
-
-# def worker_output(self, s):
-#     self.editor.setPlainText(s)
-
-# def worker_error(self, t):
-#     print("ERROR: %s" % t)
-
-# def log_tail(self, current_file):
-#     print("log_tail has been called")
-#     file_to_follow = current_file
-#     file_to_follow.seek(0, 2)
-#     while True:
-#         line = file_to_follow.readline()
-#         if not line:
-#             time.sleep(0.1)
-#             continue
-#         yield line
-
-# def search_who():
-
-#     who = []
-#     match = "players in"
-#     top_of_search = "Players on EverQuest:"
-#     end = True
-#     y = -1
-
-#     with open(latest_file, encoding='ISO-8859-1') as f:
-#         lines = f.readlines()
-#         last_line = lines[-1]
-
-#         if match in last_line:
-#             while end:
-#                 if top_of_search in lines[y]:
-#                     end = False
-#                 who.insert(0, lines[y].strip())
-#                 y -= 1
-#             return who
-
-# while go:
-#                 results = search_who()
-#                 if results != None:
-#                     go = False
-#                 if self.is_killed:
-#                     raise WorkerKilledException
-#             f = ""
-#             for i in results:
-#                 f += i + "\n"
